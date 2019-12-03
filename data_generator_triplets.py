6c59b21821136bc50a0eaf0d629e089674873ca9
Mona Lisa~ <Petar Mimica>
2019-12-03 12:41:57 +0100

    zoom-to-fill support in the data generator

--- data_generator_triplets.py
+++ data_generator_triplets.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 from AugmentedLetterbox import AugmentedLetterbox
 
-def data_generator(imgs, batch_size, loss_batch, input_shape, same_prob, no_aug_prob, no_augment=False, augment={}, greyscale=False):
+def data_generator(imgs, batch_size, loss_batch, input_shape, same_prob, no_aug_prob, no_augment=False, augment={}, greyscale=False, fill_letterbox=False):
 
     # initialize
     sizew = input_shape[0]
@@ -33,14 +33,14 @@ def data_generator(imgs, batch_size, loss_batch, input_shape, same_prob, no_aug_
                 
             lbimg_a = AugmentedLetterbox(img_a.copy())
             aug_anchor = np.random.random() >= no_aug_prob
-            lbimg_a.letterbox(sizew, sizeh, randomize_pos=aug_anchor, augments = augment if aug_anchor else None)
+            lbimg_a.letterbox(sizew, sizeh, randomize_pos=aug_anchor, augments = augment if aug_anchor else None, fill_letterbox=fill_letterbox)
             images.append(np.array(lbimg_a) / 255.0)
 
             # store the augmented positive examples
             for j in range(k//4-1):
                 lbimg_p = AugmentedLetterbox(img_a.copy())
                 aug_pos = np.random.random() >= no_aug_prob
-                lbimg_p.letterbox(sizew, sizeh, randomize_pos=aug_pos, augments = augment if aug_pos else None)
+                lbimg_p.letterbox(sizew, sizeh, randomize_pos=aug_pos, augments = augment if aug_pos else None, fill_letterbox=fill_letterbox)
                 images.append(np.array(lbimg_p) / 255.0)
 
             # store the negative examples
@@ -62,7 +62,7 @@ def data_generator(imgs, batch_size, loss_batch, input_shape, same_prob, no_aug_
                         
                 lbimg_n = AugmentedLetterbox(img_n.copy())
                 aug_neg = np.random.random() >= no_aug_prob
-                lbimg_n.letterbox(sizew, sizeh, randomize_pos=aug_neg, augments = augment if aug_neg else None)
+                lbimg_n.letterbox(sizew, sizeh, randomize_pos=aug_neg, augments = augment if aug_neg else None, fill_letterbox=fill_letterbox)
                 images.append(np.array(lbimg_n) / 255.0)
 
             # advance to next image
